91e152030f270652626db7f0259a4dd56421da06
yaothreestones <18896517552@163.com>
2017-12-23 17:19:48 +0800

    1
    1

--- mytask3/JStasklist/微信支付/weixinzhifu.js
+++ mytask3/JStasklist/微信支付/weixinzhifu.js
@@ -1,27 +1,24 @@
 angular.module('myApp',[])
-    .config(function($httpProvider){
-        $httpProvider.defaults.transformRequest = function(obj){
-            var str = [];
-            for(var p in obj){
-                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-            }
-            return str.join("&");
-        };
-        $httpProvider.defaults.headers.post = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    })
+    // .config(function($httpProvider){
+    //     $httpProvider.defaults.transformRequest = function(obj){
+    //         var str = [];
+    //         for(var p in obj){
+    //             str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    //         }
+    //         return str.join("&");
+    //     };
+    //     $httpProvider.defaults.headers.post = {
+    //         'Content-Type': 'application/x-www-form-urlencoded'
+    //     }
+    // })
     .controller('myCtrl',function ($scope,$http) {
         $scope.prepay = function () {
             $http({
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
+
                 method:'post',
                 url:'https://api.mch.weixin.qq.com/pay/unifiedorder',
-                params:{
+                dataType: 'jsonp'
 
-            }
         }).then(function (res) {
                 console.log(res)
             })
@@ -37,10 +34,19 @@ function onBridgeReady(){
             "signType":"MD5",         //微信签名方式：
             "paySign":"70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名
         },
-        function(res){
-            if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+        function (res) {
+            console.log(res.err_code + "  " + res.err_desc + "  " + res.err_msg);
+            if (res.err_msg == "get_brand_wcpay_request:ok") {
+                alertFn("充值成功");
+            } else if (res.err_msg == "get_brand_wcpay_request:cancel") {
+                alertFn("用户取消支付");
+
+            } else if (res.err_msg == "get_brand_wcpay_request:fail") {
+                alertFn("支付失败");
+            }
         }
     );
+
 }
 if (typeof WeixinJSBridge == "undefined"){
     if( document.addEventListener ){

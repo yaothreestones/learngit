ea92bd5b8ce970253a2a2a29c7be509964a2b033
yaothreestones <18896517552@163.com>
2017-12-23 22:21:06 +0800

    1
    1

---
+++ mytask3/JStasklist/project/playLearn-admin-html/trunk/ojbk/script/directives/alertModal/alertCtrl.js
@@ -0,0 +1,10 @@
+angular.module('app')
+.controller('alertCtrl', ['params', '$scope', function (params, $scope) {
+    $scope.content = params.content;
+    $scope.resolved = function () {
+        $scope.$close()
+    }
+    $scope.rejected = function () {
+        $scope.$dismiss()
+    }
+}])
---
+++ mytask3/JStasklist/project/playLearn-admin-html/trunk/ojbk/script/directives/confrimModal/confrimCtrl.js
@@ -0,0 +1,10 @@
+angular.module('app')
+.controller('confirmCtrl', ['params', '$scope', function (params, $scope) {
+    $scope.content = params.content;
+    $scope.resolved = function () {
+        $scope.$close();
+    }
+    $scope.rejected = function () {
+        $scope.$dismiss();
+    }
+}])

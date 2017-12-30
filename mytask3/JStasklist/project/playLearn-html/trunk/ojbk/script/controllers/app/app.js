47e112ab722efc4bc389d851dd4c1e65ee1639d2
yaothreestones <18896517552@163.com>
2017-12-31 00:51:49 +0800

    20171231 0:50
    1

--- mytask3/JStasklist/project/playLearn-html/trunk/ojbk/script/controllers/app/app.js
+++ mytask3/JStasklist/project/playLearn-html/trunk/ojbk/script/controllers/app/app.js
@@ -1,6 +1,8 @@
 angular.module('app')
     .controller('appCtrl',function ($scope,$state,$stateParams) {
+
         $scope.cancel = function() {
+            console.log($stateParams.there);
             if($state.params.there === '1'){
               $state.go('app.data',{'there':2})
             }else if($state.params.there === '2'){
--- mytask3/JStasklist/project/playLearn-html/trunk/ojbk/script/controllers/data/data.js
+++
@@ -1,8 +0,0 @@
-angular.module('app')
-    .controller('dataCtrl',['$scope',function ($scope) {
-        var vm = this;
-        vm.img = 'image/app/surebuy.png';
-        vm.checked = true;
-        vm.email = 'aaa@aaa.com';
-        sessionStorage.setItem('email',vm.email)
-    }])
